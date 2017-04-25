a3a09b8c40253278d8313353250b7d0685f9df25
felix021 <felix021@gmail.com>
2017-04-25 22:36:31 +0800

    download src

---
+++ php/wget.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -x
+
+#wget "https://www.openssl.org/source/openssl-1.0.2k.tar.gz"
+#wget "https://www.openssl.org/source/openssl-1.1.0e.tar.gz"
+#wget "ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.39.tar.bz2"
+
+#wget "http://nginx.org/download/nginx-1.11.10.tar.gz"
+
+#wget -O php-5.6.30.tar.gz "http://cn2.php.net/distributions/php-5.6.30.tar.gz"
+#wget -O php-7.1.2.tar.gz  "http://cn2.php.net/distributions/php-7.1.2.tar.gz"
+#wget -O composer-installer https://getcomposer.org/installer
+
+#wget http://www.memcached.org/files/memcached-1.4.25.tar.gz #server
+
+#wget https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz #for php-memcached
+
+mkdir -p php-extensions/
+cd php-extensions/
+
+#wget -O phpredis-3.1.1.tar.gz https://codeload.github.com/phpredis/phpredis/tar.gz/3.1.1
+#wget https://github.com/mongodb/mongo-php-driver/releases/download/1.1.7/mongodb-1.1.7.tgz
+#wget https://pecl.php.net/get/memcached-2.2.0.tgz
+#wget https://pecl.php.net/get/memcached-3.0.3.tgz
